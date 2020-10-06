a70c4121d50acba1a256ab29fd6a55bf9bacba71
Carla Sousa <sousa.carla09@gmail.com>
2020-10-06 13:50:07 -0300

    Add treatment the errors

--- arrayEqualize.js
+++ arrayEqualize.js
@@ -17,97 +17,124 @@ function start() {
     // coloque o array que deseja alterar;
     array = [0, 0, 4, 6, 8, 8, 4, 7, 8, 9, 9, 4, 3, 2, 3, 8, 2, 2, 1, 0];
     
-    let equalizedArray = equalize(array);
-    console.log(equalizedArray);
+    if (array == []){
+        console.log('O array está vazio.')
+    } else {
+        let equalizedArray = equalize(array);
+        console.log(equalizedArray);
+    }
 }
 
 function equalize(array) {
-    let equalizedArray = [];
-    let k_ = calculateK_(array);
-    for (let i = 0; i < array.length; i++){
-        for (let ij = 0; ij < k_.length; ij++){
-            let value = array[i];
-            if (value == ij) {
-                equalizedArray.push(k_[ij])
+    try {
+        let equalizedArray = [];
+        let k_ = calculateK_(array);
+        for (let i = 0; i < array.length; i++){
+            for (let ij = 0; ij < k_.length; ij++){
+                let value = array[i];
+                if (value == ij) {
+                    equalizedArray.push(k_[ij])
+                }
             }
         }
+        return equalizedArray;
+    } catch {
+        console.log('Não foi possível equalizar o array.');
     }
-    return equalizedArray;
 }
 
 function calculateL(array) {
-    let total = [];
-    let l;
-
-    for (let i = 0; i < array.length; i++) {
-        for (let dif = 0; dif < array.length; dif ++) {
-            if(array[i] == dif) {
-                total[dif] ++;
+    try {
+        let total = [];
+        let l;
+        for (let i = 0; i < array.length; i++) {
+            for (let dif = 0; dif < array.length; dif ++) {
+                if(array[i] == dif) {
+                    total[dif] ++;
+                }
             }
         }
+        l = total.length;
+        return l;
+    } catch {
+        console.log('Não foi possível calcular o L');
     }
-    l = total.length;
-    return l;
 }
 
 function calculateHK(array){
-    let hk = [];
-    let l = calculateL(array);
+    try {
+        let hk = [];
+        let l = calculateL(array);
 
-    for (let i = 0; i < l; i++) {
-        hk.push(0);
-    }
-    for (let i = 0; i < array.length; i++){
-        for (let equal = 0; equal < hk.length; equal ++){
-            if(array[i] == equal) {
-                hk[equal] ++;
+        for (let i = 0; i < l; i++) {
+            hk.push(0);
+        }
+        for (let i = 0; i < array.length; i++){
+            for (let equal = 0; equal < hk.length; equal ++){
+                if(array[i] == equal) {
+                    hk[equal] ++;
+                }
             }
         }
+        return hk;
+    } catch {
+        console.log('Não foi possível calcular o HK');
     }
-    return hk;
 }
 
 function calculateHAK(array){
-    let hak = [];
-    let l = calculateL(array);
-    let hk = calculateHK(array);
-
-    for (let i = 0; i < l; i++) {
-        hak.push(0);
-    }
-    for (let i = 0; i < hak.length; i++) {
-        if(i == 0) {
-            hak[0] = hk[0];
-        } else {
-            hak[i] = hak[i-1] + hk[i];
+    try {
+        let hak = [];
+        let l = calculateL(array);
+        let hk = calculateHK(array);
+    
+        for (let i = 0; i < l; i++) {
+            hak.push(0);
         }
+        for (let i = 0; i < hak.length; i++) {
+            if(i == 0) {
+                hak[0] = hk[0];
+            } else {
+                hak[i] = hak[i-1] + hk[i];
+            }
+        }
+        return hak;
+    } catch {
+        console.log('Não foi possível calcular o HAK');
     }
-    return hak;
 }
 
 function calculatePK(array){
-    let pk = [];
-    let n;
-    let hak = calculateHAK(array);
-
-    n = array.length;
-    for (let i = 0; i < hak.length; i++) {
-        let count = hak[i]/n
-        pk.push(count);
+    try {
+        let pk = [];
+        let n;
+        let hak = calculateHAK(array);
+    
+        n = array.length;
+        for (let i = 0; i < hak.length; i++) {
+            let count = hak[i]/n
+            pk.push(count);
+        }
+        return pk;
+    } catch {
+        console.log('Não foi possível calcular o PK');
     }
-    return pk;
 }
 
 function calculateK_(array){
-    let k_ = []
-    let pk = calculatePK(array);
-
-    for (let i = 0; i < pk.length; i++) {
-        let count = 9*pk[i]
-        value = Math.round(count)
-        k_.push(value);
+    try {
+        let k_ = []
+        let pk = calculatePK(array);
+    
+        for (let i = 0; i < pk.length; i++) {
+            let count = 9*pk[i]
+            value = Math.round(count)
+            k_.push(value);
+        }
+        return k_;
+    } catch {
+        console.log('Não foi possível calcular o K_');
     }
-    return k_;
 }
 
 start();
